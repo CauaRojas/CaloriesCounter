b902a7fabf63d923e19cdaa8e9329a03edbebb67
cauarojas <cauarojas@hotmail.com>
2022-03-27 16:08:49 -0300

    created the template for a search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { useState } from 'react';
 import './App.css';
+import SearchBar from './SearchBar';
+const foodData = await import('./foodData.json');
+const foods = foodData.default.Food_Display_Table.Food_Display_Row;
 
 function App() {
-	return <h1>Hello World!</h1>;
+	return (
+		<>
+			<SearchBar />
+			<h1>Hello World!</h1>;
+		</>
+	);
 }
 
 export default App;
