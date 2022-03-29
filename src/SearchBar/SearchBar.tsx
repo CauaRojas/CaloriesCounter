89d96a1b73ef3b3a7862802d14c0411fce56b1f0
cauarojas <cauarojas@hotmail.com>
2022-03-28 22:35:47 -0300

    created a function to filter the array

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -1,10 +1,33 @@
 import './SearchBar.css';
-
+import foods from '../foods';
 export default function SearchBar() {
+	// Create a function that filters the foods array by the search term
+	const filterFoods = (searchTerm: string) => {
+		const filteredFoods = foods.filter((food) => {
+			return food.Display_Name.toLowerCase().includes(
+				searchTerm.toLowerCase()
+			);
+		});
+		return filteredFoods;
+	};
+
 	return (
 		<div className='search-bar'>
-			<input type='text' placeholder='Search' />
-			<button>Search</button>
+			<input type='text' placeholder='Search' id='search' />
+			<button
+				onClick={() => {
+					console.log(
+						filterFoods(
+							(
+								document.getElementById(
+									'search'
+								) as HTMLInputElement
+							).value
+						)
+					);
+				}}>
+				Search
+			</button>
 		</div>
 	);
 }
