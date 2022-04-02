a057cd83848ad3e32911a6255473d4d9161e9e02
cauarojas <cauarojas@hotmail.com>
2022-04-02 12:02:41 -0300

    made the search system

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -1,33 +1,78 @@
+import { useState } from 'react';
 import './SearchBar.css';
 import foods from '../foods';
-export default function SearchBar() {
-	// Create a function that filters the foods array by the search term
-	const filterFoods = (searchTerm: string) => {
-		const filteredFoods = foods.filter((food) => {
-			return food.Display_Name.toLowerCase().includes(
-				searchTerm.toLowerCase()
-			);
-		});
-		return filteredFoods;
-	};
 
+// Create a function that filters the foods array by the search term
+const filterFoods = (searchTerm: string) => {
+	const filteredFoods = foods.filter((food) => {
+		return food.Display_Name.toLowerCase().includes(
+			searchTerm.toLowerCase()
+		);
+	});
+	return filteredFoods;
+};
+
+export default function SearchBar() {
+	const [searchTerm, setSearchTerm] = useState('');
 	return (
-		<div className='search-bar'>
-			<input type='text' placeholder='Search' id='search' />
-			<button
-				onClick={() => {
-					console.log(
-						filterFoods(
+		<>
+			<div className='search-bar'>
+				<input
+					type='text'
+					placeholder='Search'
+					id='search'
+					onKeyDown={() => {
+						setSearchTerm(
 							(
 								document.getElementById(
 									'search'
 								) as HTMLInputElement
 							).value
-						)
+						);
+					}}
+				/>
+				<button
+					onClick={() => {
+						console.log(
+							filterFoods(
+								(
+									document.getElementById(
+										'search'
+									) as HTMLInputElement
+								).value
+							)
+						);
+					}}>
+					Search
+				</button>
+			</div>{' '}
+			{(document.getElementById('search') as HTMLInputElement)?.value
+				.length > 2 && (
+				<SearchResults
+					searchTerm={
+						(document.getElementById('search') as HTMLInputElement)
+							?.value
+					}
+				/>
+			)}
+		</>
+	);
+}
+
+function SearchResults({ searchTerm }: { searchTerm: string }) {
+	return (
+		<div className='search-results'>
+			<h1>Search Results</h1>
+			<ul>
+				{filterFoods(searchTerm).map((food, index) => {
+					return (
+						<li key={index}>
+							{food.Display_Name} -{' '}
+							{parseFloat(food.Calories).toFixed(2)}
+						</li>
 					);
-				}}>
-				Search
-			</button>
+				})}
+			</ul>
 		</div>
 	);
 }
